b180678a7c80e9fe1fd0583ee17895fcd52655a9
tht036 <77333776+tht036@users.noreply.github.com>
2021-02-24 16:20:21 -0800

    allow users to

--- public/js/cloudLexis.js
+++ public/js/cloudLexis.js
@@ -1,26 +1,105 @@
 var clickCount = 0;
-function changeWordColor(){
+function changeWordColor1(){
   if (clickCount%2 == 0){
-    $( ".words button" ).css("background", "#FFC2AA");
+    $( "button#word1" ).css("background", "#FFC2AA");
     clickCount += 1;
   }
   else if (clickCount%2 == 1){
-    $( ".words button" ).css("background", "#BEE3DB");
+    $( "button#word1" ).css("background", "#BEE3DB");
     clickCount += 1;
   }
 }
 
-$(".words button").click(changeWordColor);
+$("button#word1").click(changeWordColor1);
 
-/*
-function customToggle(){
-    $(".customList").css("display","none");
+function changeWordColor2(){
+  if (clickCount%2 == 0){
+    $( "button#word2" ).css("background", "#FFC2AA");
+    clickCount += 1;
+  }
+  else if (clickCount%2 == 1){
+    $( "button#word2" ).css("background", "#BEE3DB");
+    clickCount += 1;
+  }
 }
-$(".customList").click(customToggle);
 
+$("button#word2").click(changeWordColor2);
 
-$("button #word9").click(function(){
-  $("ul#customList").toggle();
-})
-*/
+function changeWordColor3(){
+  if (clickCount%2 == 0){
+    $( "button#word3" ).css("background", "#FFC2AA");
+    clickCount += 1;
+  }
+  else if (clickCount%2 == 1){
+    $( "button#word3" ).css("background", "#BEE3DB");
+    clickCount += 1;
+  }
+}
+
+$("button#word3").click(changeWordColor3);
+
+function changeWordColor4(){
+  if (clickCount%2 == 0){
+    $( "button#word4" ).css("background", "#FFC2AA");
+    clickCount += 1;
+  }
+  else if (clickCount%2 == 1){
+    $( "button#word4" ).css("background", "#BEE3DB");
+    clickCount += 1;
+  }
+}
+
+$("button#word4").click(changeWordColor4);
+
+function changeWordColor5(){
+  if (clickCount%2 == 0){
+    $( "button#word5" ).css("background", "#FFC2AA");
+    clickCount += 1;
+  }
+  else if (clickCount%2 == 1){
+    $( "button#word5" ).css("background", "#BEE3DB");
+    clickCount += 1;
+  }
+}
+
+$("button#word5").click(changeWordColor5);
+
+function changeWordColor6(){
+  if (clickCount%2 == 0){
+    $( "button#word6" ).css("background", "#FFC2AA");
+    clickCount += 1;
+  }
+  else if (clickCount%2 == 1){
+    $( "button#word6" ).css("background", "#BEE3DB");
+    clickCount += 1;
+  }
+}
+
+$("button#word6").click(changeWordColor6);
+
+function changeWordColor7(){
+  if (clickCount%2 == 0){
+    $( "button#word7" ).css("background", "#FFC2AA");
+    clickCount += 1;
+  }
+  else if (clickCount%2 == 1){
+    $( "button#word7" ).css("background", "#BEE3DB");
+    clickCount += 1;
+  }
+}
+
+$("button#word7").click(changeWordColor7);
+
+function changeWordColor8(){
+  if (clickCount%2 == 0){
+    $( "button#word8" ).css("background", "#FFC2AA");
+    clickCount += 1;
+  }
+  else if (clickCount%2 == 1){
+    $( "button#word8" ).css("background", "#BEE3DB");
+    clickCount += 1;
+  }
+}
+
+$("button#word8").click(changeWordColor8);
 
